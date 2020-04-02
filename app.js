9189f1f284bbaceae85b30bc1b71cf3924232239
Erik Flores <erik.flores@colorado.edu>
2020-04-02 16:25:50 -0600

    leetcode coding challenge day 2

--- app.js
+++ app.js
@@ -159,11 +159,45 @@
 //input: [4,1,2,1,2]
 //output: 4
 
-const singleNumber = function(nums) {
-  let a = 0
-  nums.forEach((i) => {
-      a^=i
-  })
+// const singleNumber = function(nums) {
+//   let a = 0
+//   nums.forEach((i) => {
+//       a^=i
+//   })
   
-  return a
-}
+//   return a
+// }
+
+
+//Happy Number
+
+//Write an algorithm to determine if a number is "happy".
+
+//A happy number is a number defined by the following process: Starting with any positive integer, 
+//replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 
+//(where it will stay), or it loops endlessly in a cycle which 
+//does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+//Ex: input 9
+//Output: true
+//Explanation:
+// 12 + 92 = 82 * (its 1 squared and 9 squared etc.)
+// 82 + 22 = 68  (8 squared and 2 squared)
+// 62 + 82 = 100
+// 12 + 02 + 02 = 1
+
+const isHappy = function(n, counter = 0) {
+  result = false
+ if(counter < 8){
+   let array = n.toString().split('').map(n => n*n)
+   let sum = array.reduce((accumulator, currentNum) => accumulator + currentNum, 0)
+   if(sum === 1) {
+       return result = true
+   }else {
+       counter++
+       isHappy(sum, counter)
+   }
+     
+ }  
+   return result
+};
