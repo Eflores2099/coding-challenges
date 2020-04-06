deab05b67d718f1a20a1df1ee8e07db204e4f756
Erik Flores <erik.flores@colorado.edu>
2020-04-05 19:39:46 -0600

    coding challenge

--- app.js
+++ app.js
@@ -238,14 +238,39 @@
 
 // 1. You must do this in-place without making a copy of the array
 // 2. Minimize the total number of operations
-const moveZeroes = function(nums) {
+// const moveZeroes = function(nums) {
     
-for(let i = nums.length-1;i >=0; i--) {
-    if(nums[i]=== 0) {
-        nums.splice(i, 1)
-        nums.push(0)
-    }
-}
-return nums
+// for(let i = nums.length-1;i >=0; i--) {
+//     if(nums[i]=== 0) {
+//         nums.splice(i, 1)
+//         nums.push(0)
+//     }
+// }
+// return nums
    
+// };
+
+// Say you have an array for which the ith element is the price of a given stock on day i.
+
+// Design an algorithm to find the maximum profit. You may complete as many transactions as you like (i.e., buy one and sell one share of the stock multiple times).
+
+// Note: You may not engage in multiple transactions at the same time (i.e., you must sell the stock before you buy again).
+
+
+// Input: [7,1,5,3,6,4]
+// Output: 7
+// Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+//              Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+
+const maxProfit = function(prices) {
+    let profit = 0
+    
+    for(let i = 1; i < prices.length; i++) {
+        if(prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices [i - 1]
+        }
+        
+    }  
+    
+        return profit
 };
